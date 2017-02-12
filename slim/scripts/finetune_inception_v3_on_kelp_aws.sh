fa9903b599a619987aa649972e90dc9daf0f306f
Oscar <opizarro73@gmail.com>
2017-02-13 08:29:18 +1100

    removed comments that throw off arg passing

--- slim/scripts/finetune_inception_v3_on_kelp_aws.sh
+++ slim/scripts/finetune_inception_v3_on_kelp_aws.sh
@@ -47,12 +47,10 @@ python train_image_classifier.py \
   --max_number_of_steps=1000 \
   --batch_size=32 \
   --learning_rate=0.01 \
-  #--learning_rate_decay_type=fixed \
   --save_interval_secs=60 \
   --save_summaries_secs=60 \
   --log_every_n_steps=100 \
   --optimizer=rmsprop \
-  #--weight_decay=0.00004
   --weight_decay=0.999
 
 # Run evaluation.
@@ -75,12 +73,10 @@ python train_image_classifier.py \
   --max_number_of_steps=10000 \
   --batch_size=32 \
   --learning_rate=0.001 \
-#  --learning_rate_decay_type=fixed \
   --save_interval_secs=60 \
   --save_summaries_secs=60 \
   --log_every_n_steps=10 \
   --optimizer=rmsprop \
-  #--weight_decay=0.00004
   --weight_decay=0.999
 
 # Run evaluation.
